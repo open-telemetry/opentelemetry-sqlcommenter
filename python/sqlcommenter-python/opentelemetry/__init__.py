aea945c07d2fe8712228ead9a95c23d485b3972e
Thiyagu55 <thiyagu.nataraj@searce.com>
2022-01-07 16:07:23 +0530

    PR changes

--- python/sqlcommenter-python/opentelemetry/__init__.py
+++ python/sqlcommenter-python/opentelemetry/__init__.py
@@ -1,5 +1,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- __import__('pkg_resources').declare_namespace(__name__)
+__import__('pkg_resources').declare_namespace(__name__)
  
